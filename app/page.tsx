31e081622dd546470d9ab0cf807e5c82627cb60c
Hayden Zeller <me@haydenzeller.dev>
2024-02-01 19:42:08 -0500

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,86 @@
-import Image from 'next/image'
-
+import Image from "next/image"
+import Link from "next/link"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="m-0 p-0 w-full h-full bg-[#FFE7E7] flex flex-col justify-center items-center">
+      {/* this div is the main logo, slogan and the undeline */}
+      <div className="flex flex-col justify-center items-center pt-5">
+        <Image
+          src="/allscherry-logo.png"
+          alt="Allscherry-logo"
+          width={300}
+          height={300}
+        />
+        <h1 className="text-xl text-center font-bold text-black ">Feel the Wonder,<br/>Crave Adventure.</h1>
+        {/* using this styled div as an underline */}
+        <div className="h-2 w-52 rounded-lg bg-[#8B3D45]"></div>
+      </div>
+      {/* this div the the link bar each link is in its own div so the underline will be below*/}
+      <div className="flex flex-row justify-center items-center pt-10 gap-10">
+        <div className="text-center">
+          <Link href="#">
+            <h1 className="text-xl font-bold text-black hover:text-white">Shop</h1>
+            <div className="h-2 w-20 rounded-lg bg-[#8B3D45]"></div>
+          </Link>
+        </div>
+        <div className="text-center">
+          <Link href="#">
+            <h1 className="text-xl font-bold text-black ">Projects</h1>
+            <div className="h-2 w-24 rounded-lg bg-[#8B3D45]"></div>
+          </Link>
+        </div>
+        <div className="text-center">
+          <Link href="#">
+            <h1 className="text-xl font-bold text-black ">Contact</h1>
+            <div className="h-2 w-24 rounded-lg bg-[#8B3D45]"></div>
+          </Link>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* this div is the main text box */}
+      <div className="flex flex-col w-96 justify-center items-center mt-10 p-5 bg-[#F16171] rounded-xl border-b-8 border-[#8B3D45]">
+        <h1 className="text-2xl font-bold text-white">Welcome to Allscherry</h1> 
+        <p className="text-center text-white">We are a small team of designers and developers, who help brands with big ideas.</p>
+        <p className="text-center text-white">We work with clients to create digital products and services and bring them to market.</p>
+      </div>
+      {/* this div is the bubble link row */}
+      <div className="flex flex-row gap-6 pt-10 justify-center items-center">
+        <Link href="#">
+          <Image src="/linktree-footer.png" alt="linktree-bubble" width={60} height={60} />
+        </Link>
+        <Link href="#">
+          <Image src="/mail-footer.png" alt="mail-bubble" width={60} height={60} />
+        </Link>
+        <Link href="#">
+          <Image src="/mailchimp-footer.png" alt="mailchimp-bubble" width={60} height={60} />
+        </Link>
+      </div>
+      {/* this div is for erics photo */}
+      <div className="flex flex-col justify-center items-center pt-10">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/eric.png"
+          alt="Eric"
+          width={200}
+          height={200}
+          className="rounded-full"
+        />
+        <Image
+          src="/eric-bubbles.png"
+          alt="Eric-Bubbles"
+          width={300}
+          height={300}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* this div is for the expanded info double text boxes */}
+      <div className="flex flex-col w-full justify-center items-center pt-10 bg-[#DCA3A3]">
+        <div className="flex flex-col w-96 justify-center text-center items-center p-5 bg-[#F16171] rounded-xl border-b-8 border-[#8B3D45]">
+          <h1 className="text-2xl font-bold text-white">Welcome to a world of possibility</h1> 
+          <p className="text-white">My name is Eric Olcsvary, a 23 year old artist with a passion to create things, ranging from logos to websites, music and comic books.</p>
+          <p className="text-white">But enough about me, let's talk about you, and why you're here!</p>
+        </div>
+        <div className="flex flex-col w-96 justify-center text-center items-center p-5 bg-[#A85E65] mt-10 rounded-xl border-b-8 border-[#62373B]">
+          <h1 className="text-white">Scrolling down you will see a list of my favorite projects I've worked on, ones that I'm sure will make you smile.</h1> 
+          <p className="text-white">If you have any questions, or wish to work with me on a collaborative project, shoot me an email!</p>
+        </div>
       </div>
     </main>
   )
